a3d3de48cec8e7f6b555ec05ce96912a703471a3
Mikael Klevengård <mickekleven@outlook.com>
2022-05-10 16:04:43 +0200

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,15 @@
+﻿namespace RestaurantProj
+{
+    public class Employee : People
+    {
+        public int Salary { get; set; }
+    }
+
+
+    public abstract class People
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,118 @@
+﻿
+namespace RestaurantProj
+{
+    /// <summary>
+    /// Employees add or listing application
+    /// </summary>
+    static class Program
+    {
+        private static List<Employee> employees = new();
+
+
+        /// <summary>
+        /// Main entrance. Quick and dirty. 
+        /// I know, to many responsibilities but it works for now
+        /// </summary>
+        static void Main()
+        {
+            var isAsk = true;
+
+            int _salary = 0;
+
+            while (isAsk)
+            {
+
+                if (employees.Any())
+                {
+                    Console.WriteLine("Antal poster som har lagts till: {0}", employees.Count());
+                }
+
+                Console.Write("Anställd - Ange förnamn: ");
+                var firstName = Console.ReadLine();
+
+                Console.Write("Anställd - Ange efternamn: ");
+                var lastName = Console.ReadLine();
+
+                Console.Write("Anställd - Ange lön (Endast numeriska värden) : ");
+                var salary = Console.ReadLine();
+
+
+                if (!string.IsNullOrEmpty(firstName) && !string.IsNullOrEmpty(lastName) && int.TryParse(salary, out _salary))
+                {
+                    employees.Add(new Employee { FirstName = firstName, LastName = lastName, Salary = _salary });
+                }
+
+                Console.Write("Vill du fortsätta och mata in anställda? (ja) n=nej");
+                var continueAsk = Console.ReadLine();
+                if (string.IsNullOrEmpty(continueAsk) || continueAsk.ToLower().Equals("ja"))
+                {
+                    Console.Clear();
+                }
+                else
+                {
+                    isAsk = false;
+                }
+            }
+
+
+            var isAskEmpInfo = true;
+
+            while (isAskEmpInfo)
+            {
+                Console.Write("Visa information om anställd: (alla) eller enge namn: ");
+                var empInfo = Console.ReadLine();
+                ShowEmpInfo(empInfo);
+
+                Console.Write("Fortsätta att kontrollera anställda?: (ja) e=exit");
+                var empContAsk = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(empContAsk))
+                {
+                    Console.Clear();
+                }
+                else
+                {
+                    Environment.Exit(0);
+                }
+
+            }
+        }
+
+        /// <summary>
+        /// Show employee data
+        /// </summary>
+        /// <param name="_empInfo"></param>
+        static void ShowEmpInfo(string _empInfo = "")
+        {
+            List<Employee> _tempCollection = new();
+
+            if (string.IsNullOrEmpty(_empInfo))
+            {
+                _tempCollection = employees.Where(e => e.LastName.ToLower().Contains(_empInfo) ||
+                                       e.LastName.ToLower().Contains(_empInfo)).ToList();
+            }
+            else
+            {
+                _tempCollection = employees;
+            }
+
+            foreach (var empData in _tempCollection)
+            {
+                Console.WriteLine($"FirstName: {empData.FirstName} | FirstName: {empData.LastName} | Salary: {empData.Salary} sek");
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
